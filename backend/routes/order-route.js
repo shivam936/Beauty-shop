f18feb287ab0844a775313420839a547b681560f
Shivam pathak <shivampathak7789@gmail.com>
2025-05-14 14:40:59 +0530

    updation

--- backend/routes/order-route.js
+++ backend/routes/order-route.js
@@ -1,16 +1,17 @@
 import express from "express"
 import { createOrder, deleteOrder, getAllOrders, getOrderUser, updateOrder } from "../controllers/order-controller.js"
+import { protectRoute } from "../Middlewares/auth-middleware.js";
 
 const router = express.Router()
 
 router.post("/create", createOrder);
 
-router.put("/uodate/:id", updateOrder)
+router.put("/update/:id", updateOrder)
 
 router.delete("/delete/:id", deleteOrder);
 
 router.get("/:id", getOrderUser)
 
-router.get("/", getAllOrders);
+router.get("/", protectRoute, getAllOrders);
 
 export default router;
